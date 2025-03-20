1edd47a3a7ae9d198687f7e9c20c599e91fa8ef3
Sarita074 <patelsarita1556@gmail.com>
2025-03-20 21:33:06 +0530

    Solving some bugs

--- home.js
+++ home.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/${folder}/`)
+    let a = await fetch(`${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -60,7 +60,7 @@ async function getSongs(folder) {
 }
 
 const playMusic = (track, pause = false) => {
-    currentSong.src = `/${currFolder}/` + track
+    currentSong.src = `${currFolder}/` + track
     if (!pause) {
         currentSong.play()
         play.src = "pause.svg"
@@ -73,7 +73,8 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/songs/`)
+    let a = await fetch(`songs/`)
+    console.log(a)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -85,7 +86,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs/") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[1]
             // Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`)
+            let a = await fetch(`songs/${folder}/info.json`)
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
@@ -96,7 +97,7 @@ async function displayAlbums() {
                 </svg>
             </div>
 
-            <img src="/songs/${folder}/cover.jpg" alt="">
+            <img src="songs/${folder}/cover.jpg" alt="">
             <h2>${response.title}</h2>
             <p>${response.description}</p>
         </div>`
@@ -109,13 +110,12 @@ async function displayAlbums() {
             let folder = item.currentTarget.dataset.folder; // FIXED
     
             // Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`);
+            let a = await fetch(`songs/${folder}/info.json`);
             let response = await a.json(); 
             console.log(response);
             console.log("Fetching Songs");
-    
             songs = await getSongs(`songs/${folder}`); // FIXED
-    
+            playMusic(songs[0])
             document.querySelector(".folder").innerHTML = `<h2>${response.title}</h2>`; // FIXED
             document.getElementById("playbar").style.display = "block";
         });
@@ -170,9 +170,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 async function main() {
-
-    
-    
     
     
     // Get the list of all the songs
